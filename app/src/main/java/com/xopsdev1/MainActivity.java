be0a0655421a0332fbb7d041b9c1dec370b798cd
MahendraDube <dube_mahendra@hotmail.com>
2020-12-08 20:50:15 +0530

    Commit after creation of Text Button with onClick method to open another application window

--- app/src/main/java/com/xopsdev1/MainActivity.java
+++ app/src/main/java/com/xopsdev1/MainActivity.java
@@ -2,13 +2,25 @@ package com.xopsdev1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton =
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonOne = findViewById(R.id.buttonOne);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent activity2Intent = new Intent(getApplicationContext(), NewActivity.class);
+                startActivity(activity2Intent);
+            }
     }
 }
